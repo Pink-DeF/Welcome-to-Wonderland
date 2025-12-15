91730bbb742b3bdee5298424eb78f409b9d18167
Pink-deF <toni_tarasov_06@mail.ru>
2025-12-15 10:33:02 +0300

    Last add cpp

---
+++ main/enemy/cpp/MasterOfPuppet.cpp
@@ -0,0 +1,76 @@
+#include "../MasterOfPuppet.h"
+
+void MasterOfPuppet::getMovePosition()
+{
+    std::vector<std::pair<size_t, float>> weights;
+
+    switch(_data->enemyPosition[1])
+    {
+        case 3:
+            weights = { {8, 0.8f}, {5, 0.2f} };
+            break;
+        case 7:
+            weights = { {3, 0.5f}, {8, 0.3f}, {5, 0.2f} };
+            break;
+        case 8:
+            weights = { {7, 0.8f}, {5, 0.2f} };
+            break;
+        case 5:
+            weights = { {10, 0.6f}, {7, 0.3f} };
+            for(auto i: _data->enemyPosition)
+            {
+                if(i == 10)
+                {
+                    weights[1].second += weights[1].second / 2;
+                    weights[2].second += weights[1].second / 2;
+                    weights[0].second = 0.0f;
+                    break;
+                }
+            }
+            break;
+        default:
+            weights = { {3, 1.0f} };
+            break;
+    }
+
+    _data->enemyPosition[_ID] = selectPosition(weights, _data->enemyPosition[1]);
+}
+
+void MasterOfPuppet::useFake()
+{
+    _data->MasterFakePosition = static_cast<size_t>(rand() % 30);
+}
+
+MasterOfPuppet::MasterOfPuppet(std::shared_ptr<nightDB> data, size_t ID)
+{
+    _ID = ID;
+    _data = data;
+    _enemyActive = config.getNight() > 3 ? 1 : 0;
+
+    _aggression = 0.5f;
+    _speed = 0.7f;
+    setupNightParameters();
+}
+
+bool MasterOfPuppet::move(bool doorStatus)
+{
+    if(_enemyActive)
+    {
+        if(_moveCooldawn > getMoveCooldawn() + rand() % 1000)
+        {
+            _moveCooldawn = 0;
+            if(_data->enemyPosition[_ID] == 10)
+            {
+                _data->enemyPosition[_ID] = 0;
+                return doorStatus;
+            }
+            getMovePosition();
+            useFake();
+        }
+        else
+        {
+            _moveCooldawn++;
+        }
+    }
+    return 1; 
+}
---
+++ main/enemy/cpp/SpringTime.cpp
@@ -0,0 +1,69 @@
+#include "../SpringTime.h"
+
+void SpringTime::getMovePosition()
+{
+    std::vector<std::pair<size_t, float>> weights;
+
+    switch(_data->enemyPosition[_ID])
+    {
+        case 0:
+            weights = { {5, 0.3f}, {7, 0.7f} };
+            break;
+        case 5:
+            weights = { {6, 0.6f}, {9, 0.2f} };
+            break;
+        case 6:
+            _data->energy = -3000;
+            _data->enemyPosition[0] = 0;
+            break;
+        case 7:
+            weights = { {0, 0.3f}, {9, 0.7f} };
+            break;
+        case 9:
+            weights = { {10, 0.6f}, {7, 0.3f}, {6, 0.1f} };
+            for(auto i: _data->enemyPosition)
+            {
+                if(i == 10)
+                {
+                    weights[1].second += weights[0].second / 2;
+                    weights[2].second += weights[0].second / 2;
+                    weights[0].second = 0.0f;
+                    break;
+                }
+            }
+            break;
+        default:
+            weights = { {0, 1.0f} };
+            break;
+    }
+
+    _data->enemyPosition[_ID] = selectPosition(weights, _data->enemyPosition[0]);
+}
+
+SpringTime::SpringTime(std::shared_ptr<nightDB> data, size_t ID)
+{
+    _ID = ID;
+    _data = data;
+    _enemyActive = 1;
+
+    _aggression = 0.5f;
+    _speed = 0.3f;
+    setupNightParameters();
+}
+
+bool SpringTime::move(bool doorStatus)
+{
+    if(_enemyActive && _moveCooldawn > getMoveCooldawn() + rand() % 1000)
+    {
+        _moveCooldawn = 0;
+        if(_data->enemyPosition[_ID] == 10)
+        {
+            _data->enemyPosition[_ID] = 0;
+            return doorStatus;
+        }
+        getMovePosition();
+    }
+    else { _moveCooldawn++; }
+
+    return 1; 
+}
